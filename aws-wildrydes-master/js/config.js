d4e32f35de5c738774906917de276f3b8d8f432b
GARCIA Mathieu <mathieu.garcia@soprasteria.com>
2025-11-24 11:04:58 +0100

    new config js - invokurl

--- aws-wildrydes-master/js/config.js
+++ aws-wildrydes-master/js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'eu-west-3' 
     },
     api: {
-        invokeUrl: 'https://62o6yf8aee.execute-api.eu-west-3.amazonaws.com/prod',
+        invokeUrl: 'https://z1jsukfgd1.execute-api.eu-west-3.amazonaws.com/prod', 
     }
 };
